d370380d93099de27b2040375c000ddbdc20075f
amankr-08 <146210588+amankr-08@users.noreply.github.com>
2023-10-14 00:09:47 +0530

    Update WordCounter.java
    separate method added for countword....

--- WordCounter.java
+++ WordCounter.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
+
 public class WordCounter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a text or provide a file to count the words: ");
         String input = scanner.nextLine();
-        String[] words = input.split("\\s+");
-        int wordCount = 0;
-
-        for (String word : words) {
-            wordCount++;
-        }
-
+        int wordCount = countWords(input);
         System.out.println("Total count of words: " + wordCount + ".");
-}
+    }
+
+    public static int countWords(String input) {
+        String[] words = input.split("\\s+");
+        return words.length;
+    }
 }
